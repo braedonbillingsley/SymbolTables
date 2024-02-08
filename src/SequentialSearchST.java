2b62ae8a1f917e1945ed410d54372733ddff0073
braedonbillingsley <billingsley.braedon@student.greenriver.edu>
2024-02-08 13:24:05 -0800

    Added missing class from class.

---
+++ src/SequentialSearchST.java
@@ -0,0 +1,68 @@
+public class SequentialSearchST<Key, Value> implements SymbolTable<Key, Value> {
+    private Node head;
+    private int size;
+
+    private class Node {
+        Key key;
+        Value value;
+        Node next;
+
+        //constructor
+        public Node(Key key, Value value, Node next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+    }
+
+    public SequentialSearchST() {
+        head = null;
+        size = 0;
+    }
+
+    @Override
+    public void put(Key key, Value val) {
+        // if the key is not in the list, put it up front at the head
+        // but if key is already in the list, replace the value
+        //  in that node
+
+        // loop through the nodes to see if the key is there
+        Node current = head;
+        while (current != null) {
+            if (key.equals(current.key)) {
+                // we found the key! So replace the value
+                current.value = val;
+                return; // done!
+            }
+            current = current.next;
+        }
+
+        // if we got here, we know key doesn't exist in list
+        // so make a new node and put it at the front
+        head = new Node(key, val, head);
+        size++;
+    }
+
+    @Override
+    public Value get(Key key) {
+        for (Node current = head; current != null; current = current.next) {
+            if (key.equals(current.key)) {
+                return current.value;
+            }
+        }
+
+        // if I got here, I know key is not in the list
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterable<Key> keys() {
+        return null;
+    }
+}
